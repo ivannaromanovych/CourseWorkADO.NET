d2adb631c6a78178954196a64223afbb1c3b1c53
Romanovych <ivankaromanovych12@gmail.com>
2019-08-27 18:57:30 +0300

    Initial FoodDiary.DAL and folders "Abstract", "Conctrate", "Entities"

---
+++ FoodDiary/FoodDiary.DAL/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDiary.DAL.Entities
+{
+    [Table("tblUsers")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public float Height { get; set; }
+        [Required]
+        public float Weight { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public float CountOfCalories { get; set; }
+
+    }
+}
