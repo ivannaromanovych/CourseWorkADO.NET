2e149b5abf7c94d150d258f913edc77c50db05a1
Romanovych <ivankaromanovych12@gmail.com>
2019-08-28 20:09:38 +0300

    Added to FoodDiary.DAL\Abstract "IRepository.cs", to FoodDiary.DAL\Concrate "ProductRepository.cs" and "UserRepository.cs"

---
+++ FoodDiary/FoodDiary.DAL/Concrate/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using FoodDiary.DAL.Abstract;
+using FoodDiary.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDiary.DAL.Concrate
+{
+    public class ProductRepository:IRepository<Product>
+    {
+        private readonly EFContext _context = new EFContext();
+        public IEnumerable<Product> Get()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
---
+++ FoodDiary/FoodDiary.DAL/Concrate/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using FoodDiary.DAL.Abstract;
+using FoodDiary.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDiary.DAL.Concrate
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly EFContext _context = new EFContext();
+        public IEnumerable<User> Get()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
